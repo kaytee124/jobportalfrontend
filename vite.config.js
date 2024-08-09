64bcbf4f23c4cccbd39bea2ff7dc07cbb56e8fbe
kaytee124 <ktproductions124@gmail.com>
2024-08-09 17:14:59 +0000

    new ai added

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Bind to all network interfaces
-    port: 3000      // Port Vite will use
+    host: '0.0.0.0',
+    port: 3000
   }
 })
