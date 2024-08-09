4867fea44a77957f299a44576b12fb0aea319b2f
kaytee124 <ktproductions124@gmail.com>
2024-08-09 06:00:52 +0000

    works

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Bind to all network interfaces
-    port: 5173       // Port Vite will use
+    port: 3000      // Port Vite will use
   }
 })
